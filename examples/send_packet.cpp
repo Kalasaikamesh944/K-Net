b24ced30af94b2bb40cbb6db9957cc8fce2297eb
kala185 <saikamesh.y@gmail.com>
2025-03-20 07:35:55 +0530

    Update send_packet.cpp

--- examples/send_packet.cpp
+++ examples/send_packet.cpp
@@ -1,36 +1,38 @@
+/***
+MIT License
+
+Copyright (c) 2025 Kalasaikamesh944
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+***/    
 #include <iostream>
 #include <sstream>
 #include <cstdlib>
 #include <unistd.h>
 #include "knet.h"
 
-std::string getSystemStats() {
-    std::ostringstream systemData;
-    
-    // Get system uptime
-    systemData << "Uptime: ";
-    systemData << system("awk '{print $1}' /proc/uptime") << " sec | ";
-
-    // Get CPU usage
-    systemData << "CPU: ";
-    systemData << system("awk -v CONVFMT='%.2f' '{print $1+$2+$3+$4+$5+$6+$7+$8}' /proc/stat") << "% | ";
-
-    // Get Memory usage
-    systemData << "MemTotal: ";
-    systemData << system("grep MemTotal /proc/meminfo | awk '{print $2}'") << " kB | ";
-    
-    systemData << "MemFree: ";
-    systemData << system("grep MemFree /proc/meminfo | awk '{print $2}'") << " kB";
-    
-    return systemData.str();
-}
-
 int main() {
     KNet knet;
     const char* destIP = "192.168.55.16";
 
     while (true) {
-        std::string data = getSystemStats();
+        
         knet.sendKalaPacket(destIP,"This is kala Packet");
         sleep(5); // Send every 5 seconds
     }
